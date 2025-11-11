91b2f3a90ed12904c7c3a92146c370061488b4fc
SireeshaKare <kvpssireesha@gmail.com>
2025-11-11 17:29:02 +1100

    swagger installation

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pricing Service API')
+    .setDescription('API for managing pricing profiles and calculations')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(4000);
 }
 bootstrap();
